5beb60690953b886eef979bd21c16b284a1660a2
Administrator <1092738496@qq.com>
2025-02-05 06:29:18 +0800

    提交

---
+++ src/main/java/com/meditation/SySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.meditation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SySpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SySpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/meditation/SySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meditation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SySpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
